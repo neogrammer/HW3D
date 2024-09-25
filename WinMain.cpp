621998dea59f835a25a0d7f0b00da27a7e105932
neogrammer <jlhardee1986@gmail.com>
2024-09-24 23:08:59 -0400

    update

--- WinMain.cpp
+++ WinMain.cpp
@@ -1,64 +1,25 @@
 #include <Windows.h>
 #include "WindowsMessageMap.h"
 #include <wchar.h>
+#include <sstream>
+#include <string>
+#include "Window.h"
 int messageHandler();
 
 
-
-LRESULT CALLBACK WndProc(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam)
-{
-	static WindowsMessageMap mm;
-	std::string res = mm(msg, lParam, wParam);
-	std::wstring temp = std::wstring(res.begin(), res.end());
-	OutputDebugString(temp.c_str());
-	switch (msg)
-	{
-	case WM_CLOSE:
-		PostQuitMessage(69);
-		break;
-	default:
-		break;
-	}
-
-	return DefWindowProc(hWnd, msg, wParam, lParam);
-
-}
-BOOL gResult{};
-MSG e;
+MSG msg;
+BOOL gResult;
 
 int CALLBACK WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow)
 {
+	Window wnd(1366, 768, "Donkey Fart Box");
 
-	const auto pClassName = L"hw3dButts";
-	WNDCLASSEX wc = { 0 };
-	wc.cbSize = sizeof(wc);
-	wc.style = CS_OWNDC;
-	wc.lpfnWndProc = WndProc;
-	wc.cbClsExtra = 0;
-	wc.cbWndExtra = 0;
-	wc.hInstance = hInstance;
-	wc.hIcon = nullptr;
-	wc.hCursor = nullptr;
-	wc.hbrBackground = nullptr;
-	wc.lpszMenuName = nullptr;
-	wc.lpszClassName = (LPCWSTR)pClassName;
-	wc.hIconSm = nullptr;
-	//register window class
-	RegisterClassEx(&wc);
-	// create window
-	HWND hWnd = CreateWindowEx(
-		0,
-		pClassName,
-		L"Happy Hard Window",
-		WS_CAPTION | WS_SYSMENU | WS_MINIMIZEBOX,
-		200,200,640,480,
-		nullptr, nullptr,  hInstance, nullptr
-	);
-
-	ShowWindow(hWnd, SW_SHOW);
-
+	while ((gResult = GetMessage(&msg, nullptr, 0, 0)) > 0)
+	{
+		TranslateMessage(&msg);
+		DispatchMessage(&msg);
+	}
 	
-	messageHandler();
 
 	if (gResult == -1)
 	{
@@ -66,17 +27,6 @@ int CALLBACK WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLi
 	}
 	else
 	{
-		return (int)e.wParam;
+		return (int)msg.wParam;
 	}
-}
-
-int messageHandler()
-{
-	while (gResult = (GetMessage(&e, nullptr, 0, 0)) > 0)
-	{
-		TranslateMessage(&e);
-		DispatchMessage(&e);
-
-	}
-	return gResult;
 }
---
+++ Window.cpp
@@ -0,0 +1,157 @@
+#include "Window.h"
+#include <sstream>
+#include "WindowsMessageMap.h"
+
+// Window Class Stuff
+Window::WindowClass Window::WindowClass::wndClass;
+
+Window::WindowClass::WindowClass() noexcept
+	:
+	hInst(GetModuleHandle(nullptr))
+{
+	WNDCLASSEX wc = { 0 };
+	wc.cbSize = sizeof(wc);
+	wc.style = CS_OWNDC;
+	wc.lpfnWndProc = HandleMsgSetup;
+	wc.cbClsExtra = 0;
+	wc.cbWndExtra = 0;
+	wc.hInstance = GetInstance();
+	wc.hIcon = nullptr;
+	wc.hCursor = nullptr;
+	wc.hbrBackground = nullptr;
+	wc.lpszMenuName = nullptr;
+	wc.lpszClassName = (LPCWSTR)GetName();
+	wc.hIconSm = nullptr;
+	RegisterClassEx(&wc);
+}
+
+Window::WindowClass::~WindowClass()
+{
+	UnregisterClass((LPCWSTR)wndClassName, GetInstance());
+}
+
+const char* Window::WindowClass::GetName() noexcept
+{
+	return wndClassName;
+}
+
+HINSTANCE Window::WindowClass::GetInstance() noexcept
+{
+	return wndClass.hInst;
+}
+
+
+// Window Stuff
+Window::Window(int width, int height, const char* name)
+	:
+	width(width),
+	height(height)
+{
+	// calculate window size based on desired client region size
+	RECT wr;
+	wr.left = 100;
+	wr.right = width + wr.left;
+	wr.top = 100;
+	wr.bottom = height + wr.top;
+	if (AdjustWindowRect(&wr, WS_CAPTION | WS_MINIMIZEBOX | WS_SYSMENU, FALSE) == 0)
+	{
+		throw std::runtime_error("Shit");
+	}
+	// create window & get hWnd
+	hWnd = CreateWindow(
+		(LPCWSTR)WindowClass::GetName(), (LPCWSTR)name,
+		WS_CAPTION | WS_MINIMIZEBOX | WS_SYSMENU,
+		CW_USEDEFAULT, CW_USEDEFAULT, wr.right - wr.left, wr.bottom - wr.top,
+		nullptr, nullptr, WindowClass::GetInstance(), this
+	);
+	// check for error
+	if (hWnd == nullptr)
+	{
+		throw std::runtime_error("Shit");
+	}
+	SetWindowText(hWnd, L"I'm a black dude");
+	// newly created windows start off as hidden
+	ShowWindow(hWnd, SW_SHOWDEFAULT);
+
+}
+
+Window::~Window()
+{
+	/*ImGui_ImplWin32_Shutdown();*/
+	DestroyWindow(hWnd);
+}
+
+
+LRESULT CALLBACK Window::HandleMsgSetup(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam) noexcept
+{
+	// use create parameter passed in from CreateWindow() to store window class pointer at WinAPI side
+	if (msg == WM_NCCREATE)
+	{
+		// extract ptr to window class from creation data
+		const CREATESTRUCTW* const pCreate = reinterpret_cast<CREATESTRUCTW*>(lParam);
+		Window* const pWnd = static_cast<Window*>(pCreate->lpCreateParams);
+		// set WinAPI-managed user data to store ptr to window instance
+		SetWindowLongPtr(hWnd, GWLP_USERDATA, reinterpret_cast<LONG_PTR>(pWnd));
+		// set message proc to normal (non-setup) handler now that setup is finished
+		SetWindowLongPtr(hWnd, GWLP_WNDPROC, reinterpret_cast<LONG_PTR>(&Window::HandleMsgThunk));
+		// forward message to window instance handler
+		return pWnd->HandleMsg(hWnd, msg, wParam, lParam);
+	}
+	// if we get a message before the WM_NCCREATE message, handle with default handler
+	return DefWindowProc(hWnd, msg, wParam, lParam);
+}
+
+LRESULT CALLBACK Window::HandleMsgThunk(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam) noexcept
+{
+	// retrieve ptr to window instance
+	Window* const pWnd = reinterpret_cast<Window*>(GetWindowLongPtr(hWnd, GWLP_USERDATA));
+	// forward message to window instance handler
+	return pWnd->HandleMsg(hWnd, msg, wParam, lParam);
+}
+
+LRESULT Window::HandleMsg(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam) noexcept
+{
+	
+
+	static WindowsMessageMap mm;
+	std::string res = mm(msg, lParam, wParam);
+	std::wstring temp = std::wstring(res.begin(), res.end());
+	OutputDebugString(temp.c_str());
+	switch (msg)
+	{
+	case WM_CLOSE:
+		PostQuitMessage(69);
+		break;
+	case WM_KEYDOWN:
+		if (wParam == 'F')
+		{
+			SetWindowText(hWnd, L"Respects");
+		}
+		break;
+	case WM_KEYUP:
+		if (wParam == 'F')
+		{
+			SetWindowText(hWnd, L"I'm a black dude");
+		}
+		break;
+	case WM_CHAR:
+	{
+		static std::string title;
+		title.clear();
+		title.push_back((char)wParam);
+		SetWindowText(hWnd, (LPCWSTR)title.c_str());
+	}
+	break;
+	case WM_LBUTTONDOWN:
+	{
+		const POINTS pt = MAKEPOINTS(lParam);
+		SetWindowText(hWnd, (std::to_wstring((INT)pt.x) + L" " + std::to_wstring((INT)pt.y)).c_str());
+	}
+	break;
+	default:
+		break;
+	}
+
+
+	return DefWindowProc(hWnd, msg, wParam, lParam);
+}
